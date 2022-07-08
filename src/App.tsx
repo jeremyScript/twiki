3a51ad52e00a4b93691a45aae546dddd0defc052
Jeremy Lee <ultrdy@gmail.com>
2022-07-08 18:02:24 -0500

    Create and style initial code editor UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,20 @@ import React from "react";
 import styles from "./App.module.css";
 
 function App() {
-  return <div className={styles.app}>Test</div>;
+  return (
+    <div>
+      <div className={styles["code-editor"]}>
+        <textarea
+          className={styles["code-cell"]}
+          defaultValue={"Enter code here"}
+        ></textarea>
+        <button>Submit</button>
+      </div>
+      <div className={styles.result}>
+        <pre>{">>"} This is the result.</pre>
+      </div>
+    </div>
+  );
 }
 
 export default App;
