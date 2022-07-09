c6888da74d688c096eb10c450510b888514ac60e
Jeremy Lee <ultrdy@gmail.com>
2022-07-08 19:32:19 -0500

    Test and code code-cell

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import styles from "./App.module.css";
 
 function App() {
+  const codeRef = useRef<HTMLTextAreaElement>(null);
+  const [output, setOutput] = useState("");
+
+  const handleClickSubmit = () => {
+    if (codeRef.current) {
+      setOutput(codeRef.current.value);
+    }
+  };
+
   return (
     <div>
       <div className={styles["code-editor"]}>
         <textarea
           className={styles["code-cell"]}
           defaultValue={"Enter code here"}
-        ></textarea>
-        <button>Submit</button>
+          ref={codeRef}
+        />
+        <button onClick={handleClickSubmit}>Submit</button>
       </div>
-      <div className={styles.result}>
-        <pre>{">>"} This is the result.</pre>
-      </div>
+      <pre className={styles["result-cell"]}>
+        {">> "}
+        {output}
+      </pre>
     </div>
   );
 }
