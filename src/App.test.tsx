54e4f2ada5383d257100fb5b7b020b096ffc2ce0
Jeremy Lee <ultrdy@gmail.com>
2022-07-08 19:32:19 -0500

    Test and code code-cell

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,17 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
+
+test("Submit button makes string content in 'code-cell' to appear in 'result-cell' on click", () => {
+  render(<App />);
+
+  const codeCell = screen.getByRole("textbox");
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  const resultCell = screen.getByText(/>>/i);
+
+  const codeCellContent = codeCell.textContent;
+
+  userEvent.click(submitButton);
+  expect(resultCell).toHaveTextContent(">> " + codeCellContent);
+});
