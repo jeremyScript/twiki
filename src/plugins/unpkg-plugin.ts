66dda7ddd52ad597ca2c1a766ebc0f1622a190aa
Jeremy Lee <ultrdy@gmail.com>
2022-07-12 13:29:18 -0500

    Create unpkg-plugin.ts for esbuild bundling

--- src/plugins/unpkg-plugin.ts
+++ src/plugins/unpkg-plugin.ts
@@ -1,10 +1,19 @@
 import * as esbuild from "esbuild-wasm";
 
-export const unpkgPlugin = {
+export const unpkgPlugin = (inputCode: string) => ({
   name: "unpkg-plugin",
   setup(build: esbuild.PluginBuild) {
-    build.onResolve({ filter: / / }, (args) => ({}));
+    build.onResolve({ filter: /^index\.tsx$/ }, (args) => ({
+      path: args.path,
+      namespace: "a",
+    }));
 
-    build.onLoad({ filter: / / }, () => ({}));
+    build.onLoad({ filter: /^index\.tsx$/ }, () => {
+      console.log("input", inputCode);
+      return {
+        contents: inputCode,
+        loader: "tsx",
+      };
+    });
   },
-};
+});
