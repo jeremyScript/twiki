9471373abd2f8e5bfa75608c6f5682891bb0e737
Jeremy Lee <ultrdy@gmail.com>
2022-07-12 15:20:40 -0500

    Add logic for dynamic fetching

--- src/plugins/unpkg-plugin.ts
+++ src/plugins/unpkg-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPlugin = (inputCode: string) => ({
   name: "unpkg-plugin",
@@ -9,9 +10,25 @@ export const unpkgPlugin = (inputCode: string) => ({
     }));
 
     build.onLoad({ filter: /^index\.tsx$/ }, () => {
-      console.log("input", inputCode);
       return {
         contents: inputCode,
+        loader: "tsx",
+      };
+    });
+
+    build.onResolve({ filter: /^[a-z]+[-]*[a-z-]*$/ }, (args) => {
+      return {
+        path: args.path,
+        namespace: "b",
+      };
+    });
+
+    build.onLoad({ filter: /^[a-z]+[-]*[a-z-]*$/ }, async (args) => {
+      const pkgUrl = "https://www.unpkg.com/" + args.path;
+      const { data } = await axios.get(pkgUrl);
+
+      return {
+        contents: data,
         loader: "tsx",
       };
     });
