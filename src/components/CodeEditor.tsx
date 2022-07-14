82c4bf204fed85f20915fff4c95353ec8b0cd6bf
Jeremy Lee <ultrdy@gmail.com>
2022-07-14 18:08:34 -0500

    Integrate CodeEditor into App

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,14 +1,23 @@
-import Editor from "@monaco-editor/react";
+import Editor, { OnChange } from "@monaco-editor/react";
 import styles from "./CodeEditor.module.css";
 
 interface CodeEditorProps {
   initialValue: string;
+  handleInputChange: (value: string) => void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({
+  initialValue,
+  handleInputChange,
+}) => {
+  const handleEditorChange: OnChange = (value: any, event: any) => {
+    handleInputChange(value);
+  };
+
   return (
     <Editor
       value={initialValue}
+      onChange={handleEditorChange}
       height="500px"
       theme="vs-dark"
       language="javascript"
