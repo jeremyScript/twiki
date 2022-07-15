c83718f905101251077f66989eab997dd359ca27
Jeremy Lee <ultrdy@gmail.com>
2022-07-15 17:25:12 -0500

    Combine components into CodeCell

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import React, { useState } from "react";
+import CodeCell from "./components/CodeCell";
 
-import Preview from "./components/Preview";
-import CodeEditor from "./components/CodeEditor";
-import bundle from "./bundler";
 import styles from "./App.module.css";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [output, setOutput] = useState("");
-
-  const handleInputChange = (inputCode: string) => {
-    setInput(inputCode);
-  };
-
-  const handleClickSubmit = async () => {
-    const bundled = await bundle(input);
-    setOutput(bundled);
-  };
-
   return (
     <div>
-      <div className={styles["code-editor"]}>
-        <button onClick={handleClickSubmit}>Submit</button>
-      </div>
-      <CodeEditor
-        initialValue="const a = 123;"
-        handleInputChange={handleInputChange}
-      />
-      <Preview code={output} />
-      <pre className={styles["result-cell"]}>
-        {">> "}
-        {output}
-      </pre>
+      <CodeCell />
+      <CodeCell />
     </div>
   );
 }
