2e379ef5799bfb92762d8890f1972ed8d42329f0
Jeremy Lee <ultrdy@gmail.com>
2022-07-25 13:48:56 -0500

    Clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import CodeCell from "./components/code-cell/CodeCell";
 import PageTitle from "./components/PageTitle";
 import TextEditor from "./components/text-cell/TextEditor";
+import Main from "./components/Main";
 
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.App}>
-      <PageTitle />
-      <TextEditor />
-      <CodeCell />
+      <Main>
+        <PageTitle />
+        <TextEditor />
+        <CodeCell />
+      </Main>
     </div>
   );
 }
