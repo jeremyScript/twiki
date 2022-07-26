d06f6f080508601167dc6d3a7560cada274fb78d
Jeremy Lee <ultrdy@gmail.com>
2022-07-26 12:12:58 -0500

    Lift state up

--- src/components/header/MenuIcon.tsx
+++ src/components/header/MenuIcon.tsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
-
 import styles from "./MenuIcon.module.css";
 
-const MenuIcon: React.FC = () => {
-  const [showMenu, setShowMenu] = useState(false);
+interface MenuIconProps {
+  showMenu: boolean;
+  handleMenuClick: () => void;
+}
 
-  const handleClick = () => {
-    setShowMenu((prevIsMenuOpen) => !prevIsMenuOpen);
-  };
-
+const MenuIcon: React.FC<MenuIconProps> = ({ showMenu, handleMenuClick }) => {
   const classOnToggle = showMenu ? styles.change : "";
 
   return (
     <div
       className={styles["menu-icon"]}
-      onClick={handleClick}
+      onClick={handleMenuClick}
       aria-label="menu-control"
     >
       <div className={`${styles.bar1} ${classOnToggle}`} />
