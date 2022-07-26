115f1ea4530f7695ea81106ba93b292bfe57c8f5
Jeremy Lee <ultrdy@gmail.com>
2022-07-26 11:51:14 -0500

    Create and add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import Header from "./components/header/Header";
 import CodeCell from "./components/code-cell/CodeCell";
 import PageTitle from "./components/PageTitle";
 import TextEditor from "./components/text-cell/TextEditor";
 import Main from "./components/Main";
 
 import styles from "./App.module.css";
-import MenuIcon from "./components/header/MenuIcon";
 
 function App() {
   return (
     <div className={styles.App}>
-      <MenuIcon />
+      <Header />
       <Main>
         <PageTitle />
         <TextEditor />
