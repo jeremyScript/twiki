b15e7d97d6897fe30a988fbc7ee252d045a1c607
Jeremy Lee <ultrdy@gmail.com>
2022-07-26 16:04:10 -0500

    Add animation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+
 import MenuIcon from "./MenuIcon";
 import Modal from "../Modal";
 
@@ -8,33 +10,42 @@ const Header: React.FC = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   const handleMenuClick = () => {
-    setShowMenu((prevShowMenu) => !prevShowMenu);
+    setShowMenu(!showMenu);
   };
 
   return (
     <header className={styles.header}>
       <MenuIcon showMenu={showMenu} handleMenuClick={handleMenuClick} />
-      {showMenu && (
-        <Modal handleClick={handleMenuClick}>
-          <nav className={styles["sliding-menu"]}>
-            <ul className={styles["menu-contents"]}>
-              <li>
-                <button>&#43; New</button>
-              </li>
-              <li>
-                <button>
-                  <img src="./save.svg" height={13} alt="save" /> Save
-                </button>
-              </li>
-              <li>
-                <button>
-                  <img src="./load.svg" height={13} alt="load" /> Load
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </Modal>
-      )}
+      {showMenu && <Modal handleClick={handleMenuClick} />}
+      <CSSTransition
+        in={showMenu}
+        timeout={300}
+        unmountOnExit
+        classNames={{
+          enter: styles["sliding-enter"],
+          enterActive: styles["sliding-enter-active"],
+          exit: styles["sliding-exit"],
+          exitActive: styles["sliding-exit-active"],
+        }}
+      >
+        <nav className={styles["sliding-menu"]}>
+          <ul className={styles["menu-contents"]}>
+            <li>
+              <button>&#43; New</button>
+            </li>
+            <li>
+              <button>
+                <img src="./save.svg" height={13} alt="save" /> Save
+              </button>
+            </li>
+            <li>
+              <button>
+                <img src="./load.svg" height={13} alt="load" /> Load
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </CSSTransition>
     </header>
   );
 };
