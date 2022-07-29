971636815553dd2cd24e933d99e8787e7abd9363
Jeremy Lee <ultrdy@gmail.com>
2022-07-29 12:57:14 -0500

    Refactor PageTitle

--- src/components/main/PageTitle.tsx
+++ src/components/main/PageTitle.tsx
@@ -2,21 +2,10 @@ import React, { useState } from "react";
 import styles from "./PageTitle.module.css";
 
 const PageTitle: React.FC = () => {
-  const [title, setTitle] = useState("(untitled)");
+  const [title, setTitle] = useState("");
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
-  };
-
-  const handleFocus = () => {
-    if (title.trim().length > 0) return;
-    setTitle("");
-  };
-
-  const handleBlur = () => {
-    if (title.trim() === "") {
-      setTitle("(untitled)");
-    }
   };
 
   return (
@@ -24,8 +13,7 @@ const PageTitle: React.FC = () => {
       <input
         type="text"
         onChange={handleChange}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
+        placeholder="(untitled)"
         className={styles["title-input"]}
         value={title}
       />
