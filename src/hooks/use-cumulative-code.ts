8fa4cc14bb9f6d5fbf44c2caff90c6d8473a8ca1
Jeremy Lee <ultrdy@gmail.com>
2022-08-04 17:48:17 -0500

    Extract code and refactor

---
+++ src/hooks/use-cumulative-code.ts
@@ -0,0 +1,46 @@
+import { useAppSelector } from "./typed-hooks";
+import { selectOrderedCells } from "../state/cellsSlice";
+
+const useCumulativeCode = (id: string) => {
+  const orderedCells = useAppSelector(selectOrderedCells);
+  const cumulativeCode: string[] = [];
+
+  const showFunc = `
+    import _React from 'react';
+    import _ReactDOM from 'react-dom';
+
+    var show = (value) => {
+      const root = document.getElementById('root');
+      if (typeof value === 'object') {
+        if (value.$$typeof && value.props) {
+          _ReactDOM.render(
+            value,
+            root
+          );
+        } else {
+          root.innerHTML = JSON.stringify(value);
+        }
+      } else {
+        root.innerHTML = value;
+      }
+    };
+  `;
+
+  const showFuncNoOp = `var show = () => {};`;
+
+  for (let cell of orderedCells) {
+    if (cell.type === "code") {
+      if (cell.id === id) {
+        cumulativeCode.push(showFunc);
+      } else {
+        cumulativeCode.push(showFuncNoOp);
+      }
+      cumulativeCode.push(cell.content);
+      if (cell.id === id) break;
+    }
+  }
+
+  return cumulativeCode.join("\n");
+};
+
+export default useCumulativeCode;
