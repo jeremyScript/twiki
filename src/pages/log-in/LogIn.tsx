415ddf0823cef3b8a5dfbaadd7ee487d6d1e2ce0
Jeremy Lee <ultrdy@gmail.com>
2022-08-10 15:14:38 -0500

    Edit LogIn

--- src/pages/log-in/LogIn.tsx
+++ src/pages/log-in/LogIn.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Main from "../../components/main/Main";
+
 import styles from "./LogIn.module.css";
 
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // TODO
+  };
+
   return (
     <Main>
-      <form className={styles["login-form"]}>
+      <form className={styles["login-form"]} onSubmit={handleSubmit}>
         <h2>Log in to your tWiki account!</h2>
         <div className={styles["email"]}>
           <input
@@ -12,11 +22,18 @@ const LogIn = () => {
             type="email"
             id="login-email"
             placeholder=" "
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label className={styles["label"]} htmlFor="login-email">
             Email
           </label>
-          <img src="./email-icon.svg" height={22} alt="email" />
+          <img
+            className={styles["icon"]}
+            src="./email-icon.svg"
+            height={22}
+            alt="email"
+          />
         </div>
         <div className={styles["password"]}>
           <input
@@ -24,14 +41,21 @@ const LogIn = () => {
             type="password"
             id="login-password"
             placeholder=" "
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <label className={styles["label"]} htmlFor="login-password">
             Password
           </label>
-          <img src="./password-icon.svg" height={22} alt="password" />
+          <img
+            className={styles["icon"]}
+            src="./password-icon.svg"
+            height={22}
+            alt="password"
+          />
         </div>
         <button className={styles["btn"]} type="submit">
-          Log In
+          Log in
         </button>
       </form>
     </Main>
