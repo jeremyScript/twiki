9986f890605ba508fe25288afe03d333876d81c6
Jeremy Lee <ultrdy@gmail.com>
2022-08-10 15:40:43 -0500

    Install Firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzNQDDpEDhuf7oTwjdNvcpBMeubWeUroI",
+  authDomain: "twiki-f8461.firebaseapp.com",
+  projectId: "twiki-f8461",
+  storageBucket: "twiki-f8461.appspot.com",
+  messagingSenderId: "765302028849",
+  appId: "1:765302028849:web:6c5ce4d8da8f389cb57310",
+};
+
+// Init Firebase
+const app = initializeApp(firebaseConfig);
+
+// Init Firestore
+const db = getFirestore(app);
+
+export { db };
