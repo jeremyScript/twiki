b127382c0e7e712565ad80b165f72a2d0c258d34
Jeremy Lee <ultrdy@gmail.com>
2022-08-10 21:54:39 -0500

    Implement log out

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,8 +1,13 @@
 import { NavLink } from "react-router-dom";
+import { useAppSelector } from "../../hooks/typed-hooks";
+import useLogOut from "../../hooks/useLogOut";
 
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
+  const isLoggedIn = Boolean(useAppSelector((state) => state.user.currentUser));
+  const { logOut } = useLogOut();
+
   return (
     <nav className={styles["nav"]}>
       <ul>
@@ -14,30 +19,41 @@ const NavBar = () => {
             Home
           </NavLink>
         </li>
-        <li className={styles["nav-item"]}>
-          <NavLink
-            to="log-in"
-            className={({ isActive }) =>
-              isActive
-                ? `${styles["isActive"]} ${styles["log-in"]}`
-                : styles["log-in"]
-            }
-          >
-            Log in
-          </NavLink>
-        </li>
-        <li className={`${styles["nav-item"]}`}>
-          <NavLink
-            to="sign-up"
-            className={({ isActive }) =>
-              isActive
-                ? `${styles["isActive"]} ${styles["sign-up"]}`
-                : styles["sign-up"]
-            }
-          >
-            Sign up
-          </NavLink>
-        </li>
+        {!isLoggedIn && (
+          <li className={styles["nav-item"]}>
+            <NavLink
+              to="log-in"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles["isActive"]} ${styles["log-in"]}`
+                  : styles["log-in"]
+              }
+            >
+              Log in
+            </NavLink>
+          </li>
+        )}
+        {!isLoggedIn && (
+          <li className={`${styles["nav-item"]}`}>
+            <NavLink
+              to="sign-up"
+              className={({ isActive }) =>
+                isActive
+                  ? `${styles["isActive"]} ${styles["sign-up"]}`
+                  : styles["sign-up"]
+              }
+            >
+              Sign up
+            </NavLink>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li className={styles["nav-item"]}>
+            <NavLink to="/" className={styles["log-out"]} onClick={logOut}>
+              Log out
+            </NavLink>
+          </li>
+        )}
       </ul>
     </nav>
   );
