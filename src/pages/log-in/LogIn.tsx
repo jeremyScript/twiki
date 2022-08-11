81a980e242f8ed8e0b9ba9584b2050ad63532668
Jeremy Lee <ultrdy@gmail.com>
2022-08-11 15:33:12 -0500

    Update pages

--- src/pages/log-in/LogIn.tsx
+++ src/pages/log-in/LogIn.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "../../hooks/useTypedHooks";
 import useLogIn from "../../hooks/useLogIn";
 import Main from "../../components/main/Main";
 
@@ -14,60 +16,65 @@ const LogIn = () => {
     logIn(email, password);
   };
 
+  const isLoggedIn = Boolean(useAppSelector((state) => state.user.currentUser));
+
   return (
     <Main>
-      <form className={styles["log-in-form"]} onSubmit={handleSubmit}>
-        <h2>Log in to your tWiki account!</h2>
-        <div className={styles["email"]}>
-          <input
-            className={styles["input"]}
-            type="email"
-            id="log-in-email"
-            placeholder=" "
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label className={styles["label"]} htmlFor="log-in-email">
-            Email
-          </label>
-          <img
-            className={styles["icon"]}
-            src="./email-icon.svg"
-            height={22}
-            alt="email"
-          />
-        </div>
-        <div className={styles["password"]}>
-          <input
-            className={styles["input"]}
-            type="password"
-            id="log-in-password"
-            placeholder=" "
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <label className={styles["label"]} htmlFor="log-in-password">
-            Password
-          </label>
-          <img
-            className={styles["icon"]}
-            src="./password-icon.svg"
-            height={22}
-            alt="password"
-          />
-        </div>
-        {!isPending && (
-          <button className={styles["btn"]} type="submit">
-            Log in
-          </button>
-        )}
-        {isPending && (
-          <button className={styles["btn"]} type="submit">
-            Logging in
-          </button>
-        )}
-        {error && <p>{error}</p>}
-      </form>
+      {isLoggedIn && <Navigate to="/" replace={true} />}
+      {!isLoggedIn && (
+        <form className={styles["log-in-form"]} onSubmit={handleSubmit}>
+          <h2>Log in to your tWiki account!</h2>
+          <div className={styles["email"]}>
+            <input
+              className={styles["input"]}
+              type="email"
+              id="log-in-email"
+              placeholder=" "
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label className={styles["label"]} htmlFor="log-in-email">
+              Email
+            </label>
+            <img
+              className={styles["icon"]}
+              src="./email-icon.svg"
+              height={22}
+              alt="email"
+            />
+          </div>
+          <div className={styles["password"]}>
+            <input
+              className={styles["input"]}
+              type="password"
+              id="log-in-password"
+              placeholder=" "
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label className={styles["label"]} htmlFor="log-in-password">
+              Password
+            </label>
+            <img
+              className={styles["icon"]}
+              src="./password-icon.svg"
+              height={22}
+              alt="password"
+            />
+          </div>
+          {!isPending && (
+            <button className={styles["btn"]} type="submit">
+              Log in
+            </button>
+          )}
+          {isPending && (
+            <button className={styles["btn"]} type="submit">
+              Logging in
+            </button>
+          )}
+          {error && <p>{error}</p>}
+        </form>
+      )}
     </Main>
   );
 };
