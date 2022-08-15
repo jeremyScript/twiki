4646ccb2863b5b618777dc4398246e90d263b1a9
Jeremy Lee <ultrdy@gmail.com>
2022-08-15 18:22:30 -0500

    Implement Modal and Documents

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import Main from "../../components/main/Main";
+import DocumentControls from "../../components/main/DocumentControls";
 import PageTitle from "../../components/main/PageTitle";
 import CellList from "../../components/main/CellList";
-import DocumentControls from "../../components/main/DocumentControls";
+import Modal from "../../components/ui/Modal";
+import Documents from "../../components/main/Documents";
+
+import styles from "./Home.module.css";
+
+const transitionStyles = {
+  enter: styles["sliding-enter"],
+  enterActive: styles["sliding-enter-active"],
+  exit: styles["sliding-exit"],
+  exitActive: styles["sliding-exit-active"],
+};
 
 const Home = () => {
+  const [showDocuments, setShowDocuments] = useState(false);
+
+  const toggleModal = () => {
+    setShowDocuments(!showDocuments);
+  };
+
   return (
     <Main>
-      <DocumentControls />
+      <DocumentControls showModal={toggleModal} />
       <PageTitle />
       <CellList />
+      {showDocuments && <Modal handleClick={toggleModal} />}
+      <CSSTransition
+        in={showDocuments}
+        timeout={300}
+        classNames={transitionStyles}
+        unmountOnExit
+      >
+        <Documents closeModal={toggleModal} />
+      </CSSTransition>
     </Main>
   );
 };
