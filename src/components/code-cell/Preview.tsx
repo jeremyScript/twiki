446a6858b5f1249c8d85df7cfc947fcc334080ad
Jeremy Lee <ultrdy@gmail.com>
2022-08-16 18:17:57 -0500

    Make stylistic changes

--- src/components/code-cell/Preview.tsx
+++ src/components/code-cell/Preview.tsx
@@ -16,6 +16,18 @@ const Preview: React.FC<PreviewProps> = ({ code, bundleStatus }) => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      *,
+      ::before,
+      ::after {
+        box-sizing: inherit;
+      }
+
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+      }
+    </style>
   </head>
   <body>
     <div id="root" />
