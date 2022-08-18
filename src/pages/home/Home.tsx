3d1f5f92e8b6d13b113da6a0afe0d22476e1e1ca
Jeremy Lee <ultrdy@gmail.com>
2022-08-18 17:11:05 -0500

    Lift state up

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
+import { useAppSelector } from "../../hooks/useTypedHooks";
 import { CSSTransition } from "react-transition-group";
+import useFireStore from "../../hooks/useFirestore";
 import Main from "../../components/main/Main";
 import DocumentControls from "../../components/main/DocumentControls";
 import PageTitle from "../../components/main/PageTitle";
 import CellList from "../../components/main/CellList";
 import Modal from "../../components/ui/Modal";
 import Documents from "../../components/main/Documents";
+import Notification from "../../components/ui/Notification";
 
 import styles from "./Home.module.css";
-import { useAppSelector } from "../../hooks/useTypedHooks";
+
+const notification: { [op: string]: { success: string; error: string } } = {
+  save: {
+    success: "Document saved",
+    error: "Document could not be saved",
+  },
+  load: {
+    success: "Document loaded",
+    error: "Document could not be loaded",
+  },
+  delete: {
+    success: "Document deleted",
+    error: "Document could not be deleted",
+  },
+};
 
 const transitionStyles = {
   enter: styles["sliding-enter"],
@@ -18,16 +35,39 @@ const transitionStyles = {
 };
 
 const Home = () => {
-  const [showDocuments, setShowDocuments] = useState(false);
   const isLoggedIn = Boolean(useAppSelector((state) => state.user.currentUser));
+  const [showDocuments, setShowDocuments] = useState(false);
+  const [operation, setOperation] = useState("");
+  const { saveDocument, deleteDocument, isPending, success, error } =
+    useFireStore();
 
   const toggleModal = () => {
     setShowDocuments(!showDocuments);
   };
 
+  const informOperationType = (type: string) => {
+    setOperation(type);
+  };
+
   return (
     <Main>
-      {isLoggedIn && <DocumentControls showModal={toggleModal} />}
+      {isLoggedIn && (
+        <DocumentControls
+          saveDocument={saveDocument}
+          deleteDocument={deleteDocument}
+          showModal={toggleModal}
+          informOperationType={informOperationType}
+        />
+      )}
+      {error && (
+        <Notification type="error" message={notification[operation]?.error} />
+      )}
+      {success && (
+        <Notification
+          type="success"
+          message={notification[operation]?.success}
+        />
+      )}
       <PageTitle />
       <CellList />
       {showDocuments && <Modal handleClick={toggleModal} />}
