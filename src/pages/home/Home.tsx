b520c9a4cae19d50263a6d0aee1acd8116a25168
Jeremy Lee <ultrdy@gmail.com>
2022-08-30 16:58:23 -0500

    Change route behavior

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
-import { useAppSelector } from "../../hooks/useTypedHooks";
+import { useEffect, useState } from "react";
+import { Navigate, useMatch } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../hooks/useTypedHooks";
 import { CSSTransition } from "react-transition-group";
+import { clearDocument, updateDocument } from "../../state/documentSlice";
+import { clearBundles } from "../../state/bundlesSlice";
 import useFireStore from "../../hooks/useFirestore";
 import Main from "../../components/main/Main";
 import DocumentControls from "../../components/main/DocumentControls";
@@ -10,7 +13,15 @@ import Modal from "../../components/ui/Modal";
 import Documents from "../../components/main/Documents";
 import Notification from "../../components/ui/Notification";
 
+import { intro } from "../../documents/intro";
 import styles from "./Home.module.css";
+
+const transitionStyles = {
+  enter: styles["sliding-enter"],
+  enterActive: styles["sliding-enter-active"],
+  exit: styles["sliding-exit"],
+  exitActive: styles["sliding-exit-active"],
+};
 
 const notification: { [op: string]: { success: string; error: string } } = {
   save: {
@@ -27,19 +38,26 @@ const notification: { [op: string]: { success: string; error: string } } = {
   },
 };
 
-const transitionStyles = {
-  enter: styles["sliding-enter"],
-  enterActive: styles["sliding-enter-active"],
-  exit: styles["sliding-exit"],
-  exitActive: styles["sliding-exit-active"],
-};
-
 const Home = () => {
   const isLoggedIn = Boolean(useAppSelector((state) => state.user.currentUser));
   const [showDocuments, setShowDocuments] = useState(false);
   const [operation, setOperation] = useState("");
   const { saveDocument, fetchDocument, deleteDocument, success, error } =
     useFireStore();
+
+  const dispatch = useAppDispatch();
+  const matchIndex = useMatch("/");
+  const matchIntro = useMatch("/intro");
+  const matchDemo = useMatch("/demo");
+
+  useEffect(() => {
+    if (matchIntro) {
+      dispatch(updateDocument(intro));
+    } else if (matchDemo) {
+      dispatch(clearDocument());
+      dispatch(clearBundles());
+    }
+  }, [dispatch, matchIntro, matchDemo]);
 
   const toggleModal = () => {
     setShowDocuments(!showDocuments);
@@ -50,43 +68,46 @@ const Home = () => {
   };
 
   return (
-    <Main>
-      {isLoggedIn && (
-        <DocumentControls
-          saveDocument={saveDocument}
-          deleteDocument={deleteDocument}
-          showModal={toggleModal}
-          informOperationType={informOperationType}
-        />
-      )}
-      {error && (
-        <Notification
-          type="error"
-          message={error || notification[operation]?.error}
-        />
-      )}
-      {success && (
-        <Notification
-          type="success"
-          message={notification[operation]?.success}
-        />
-      )}
-      <PageTitle />
-      <CellList />
-      {showDocuments && <Modal handleClick={toggleModal} />}
-      <CSSTransition
-        in={showDocuments}
-        timeout={200}
-        classNames={transitionStyles}
-        unmountOnExit
-      >
-        <Documents
-          fetchDocument={fetchDocument}
-          informOperationType={informOperationType}
-          closeModal={toggleModal}
-        />
-      </CSSTransition>
-    </Main>
+    <>
+      {matchIndex && <Navigate to="/intro" replace={true} />}
+      <Main>
+        {isLoggedIn && (
+          <DocumentControls
+            saveDocument={saveDocument}
+            deleteDocument={deleteDocument}
+            showModal={toggleModal}
+            informOperationType={informOperationType}
+          />
+        )}
+        {error && (
+          <Notification
+            type="error"
+            message={error || notification[operation]?.error}
+          />
+        )}
+        {success && (
+          <Notification
+            type="success"
+            message={notification[operation]?.success}
+          />
+        )}
+        <PageTitle />
+        <CellList />
+        {showDocuments && <Modal handleClick={toggleModal} />}
+        <CSSTransition
+          in={showDocuments}
+          timeout={200}
+          classNames={transitionStyles}
+          unmountOnExit
+        >
+          <Documents
+            fetchDocument={fetchDocument}
+            informOperationType={informOperationType}
+            closeModal={toggleModal}
+          />
+        </CSSTransition>
+      </Main>
+    </>
   );
 };
 
