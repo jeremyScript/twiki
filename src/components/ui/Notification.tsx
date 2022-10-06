cfe521720281184cf6569a4fccfa69b474e2f14a
Jeremy Lee <ultrdy@gmail.com>
2022-10-06 16:29:48 -0500

    Update hook and ui

--- src/components/ui/Notification.tsx
+++ src/components/ui/Notification.tsx
@@ -1,28 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useNotification } from "../../hooks/useNotification";
+import { useAppSelector } from "../../hooks/useTypedHooks";
+
 import styles from "./Notification.module.css";
 
 interface NotificationProps {
-  type: "success" | "error";
-  message?: string;
+  timeout?: number;
 }
 
-const Notification: React.FC<NotificationProps> = ({ type, message }) => {
-  const [show, setShow] = useState(true);
+const Notification: React.FC<NotificationProps> = ({ timeout = 3000 }) => {
+  const [displayNotification, clearNotification] = useNotification();
+
+  const { error, success } = useAppSelector((state) => state.document);
+  const { display, message } = useAppSelector((state) => state.notification);
 
   useEffect(() => {
     let timer: any;
-    if (!timer) {
-      timer = setTimeout(() => {
-        setShow(false);
-      }, 2500);
-    } else {
-      clearTimeout(timer);
+
+    if (success || error) {
+      displayNotification({
+        type: success ? "info" : "error",
+        message: success ? success : error,
+        timeout,
+      });
+      if (!timer) {
+        timer = setTimeout(() => {
+          clearNotification();
+        }, timeout);
+      } else {
+        clearTimeout(timer);
+      }
     }
-
     return () => clearTimeout(timer);
-  }, []);
+  }, [success, error, timeout, displayNotification, clearNotification]);
 
-  return <>{show && <div className={styles["notification"]}>{message}</div>}</>;
+  return (
+    <>{display && <div className={styles["notification"]}>{message}</div>}</>
+  );
 };
 
 export default Notification;
